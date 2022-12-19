8b1e3eaa974e0d65e408297675fd1874acf0be02
burny <shskse5@gmail.com>
2022-12-19 19:10:29 +0900

    12-19 이미지 넣는법 ,import export, 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,20 +1,78 @@
 import "./App.css";
-import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import bg from "./img/shoe.jpg";
+// 임포트 부터 하고 써야함 외부링크는 그냥가능
+import { useState } from "react";
+import data from "./data.js";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
-    <div className='App'>
-      <Button variant='primary'>버튼</Button>{" "}
-      <Navbar bg='dark' variant='dark'>
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
-          <Nav className='me-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#features'>Features</Nav.Link>
-            <Nav.Link href='#pricing'>Pricing</Nav.Link>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div
+        className="main-bg"
+        style={{ backgroundImage: "url(" + bg + ")" }}
+      ></div>
+
+      <div className="container">
+        <div className="row">
+          {shoes.map((a, i) => {
+            return <Product shoes={shoes[i]} i={i} />;
+          })}
+
+          {/* <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Product(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
